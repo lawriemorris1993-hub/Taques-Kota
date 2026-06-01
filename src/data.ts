3d8ec83bc0dadd5e9f546d2d881a999ffe0e371b
lawriemorris1993-hub <lawriemorris1993@gmail.com>
2026-06-01 19:32:20 +0200

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,7 @@
 import { MenuItem, Feature } from './types';
+import KasiKota from '/src/assets/images/kasi_kota_1780331989837.png';
+import classicBurger from '/src/assets/images/classic_burger_1780332010782.png';
+import KasiChips from '/src/assets/images/kasi_chips_1780332029189.png';
 
 export const WHATSAPP_NUMBER = '+263 774 675 739';
 export const WHATSAPP_RAW_LINK = 'https://wa.me/263774675739';
@@ -11,21 +14,21 @@ export const MENU_ITEMS: MenuItem[] = [
     name: 'Fresh Kasi Kota',
     price: 1.50,
     description: 'Freshly prepared Kota packed with flavour and served hot.',
-    image: '/src/assets/images/kasi_kota_1780331989837.png'
+    image: KasiKota
   },
   {
     id: 'classic-burger',
     name: 'Classic Burger',
     price: 1.00,
     description: 'Juicy burger prepared fresh and served hot.',
-    image: '/src/assets/images/classic_burger_1780332010782.png'
+    image: classicBurger
   },
   {
     id: 'kasi-chips',
     name: 'Kasi Style Chips',
     price: 1.00,
     description: 'Golden crispy chips seasoned to perfection.',
-    image: '/src/assets/images/kasi_chips_1780332029189.png'
+    image: KasiChips
   },
   {
     id: 'combo-meal',
